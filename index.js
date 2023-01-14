7196522aeab2da1b5ca766eb17b18430b33f72f4
Aniket <aniketdhokane2071@gmail.com>
2023-01-14 19:48:53 +0530

    Signup

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-import express from "express"
+import express from 'express'
 import dotenv from 'dotenv'
 import Connection from "./database/db.js"
 import DefaultData from "./defaults.js"
+import router from "./routes/route.js"
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app=express()
 
 
 dotenv.config()
+
+app.use(cors())
+// body parser used for post request
+app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use('/',router)
+
 
 const USERNAME= process.env.DB_USERNAME;
 const PASSWORD= process.env.DB_PASSWORD;
